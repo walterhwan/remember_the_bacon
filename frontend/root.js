6c159e23ba949c3f202d758fe6becbd8f7a9dc4b
Walter Wan <WalterHWan@gmail.com>
2018-04-03 13:46:10 -0700

    Fix jquery, add webpack

---
+++ frontend/root.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById('root');
+  // const store = configureStore();
+  // ReactDOM.render(<Root store={store}>, root);
+  ReactDOM.render(<h1>This is root</h1>, root);
+});
