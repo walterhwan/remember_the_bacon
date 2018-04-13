6a3c9f4222b1fb7467f7ad84a909ae0e6527df52
Walter Wan <WalterHWan@gmail.com>
2018-04-13 00:14:05 -0700

    Add landing page pic

--- frontend/components/landing_page.jsx
+++ frontend/components/landing_page.jsx
@@ -32,10 +32,8 @@ class LandingPage extends React.Component {
 
   render() {
     return (
-      <div
-        className='landing-page'>
-        <nav
-          className='landing-nav'
+      <div className='landing-page'>
+        <nav className='landing-nav'
           id='landing-nav'>
           <div >
             <Link to='/' className='logo'/>
@@ -55,8 +53,7 @@ class LandingPage extends React.Component {
             </ul>
           </div>
         </nav>
-        <main
-          className='landing-main'
+        <main className='landing-main'
           id='landing-main'>
           <div className='quote-signup-div'>
             <h1>The smart to-do app for lazy people.</h1>
@@ -65,6 +62,8 @@ class LandingPage extends React.Component {
               to='/signup'>Sign Up Free</Link>
           </div>
           <div className='silder-div'>
+            <img src='/lazy.svg' />
+            <div className='svg-author'>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
           </div>
         </main>
         <footer className='landing-footer'>
--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
     path: path.resolve(__dirname, "app", "assets", "javascripts"),
     filename: "bundle.js"
   },
-  // plugins: plugins,
+  plugins: plugins,
   module: {
     loaders: [
       {
